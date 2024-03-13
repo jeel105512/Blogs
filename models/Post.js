93c2d111ff5c3bfecc89c5b38cc6b028d9170381
jeel105512 <jeelhpatel10@gmail.com>
2024-03-13 15:27:16 -0400

    added models Post.js and Comment.js

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: [true, "user-id is required"],
+    },
+    title: {
+      type: String,
+      required: [true, "post must have a title"],
+    },
+    image: {
+      type: String,
+      required: false,
+      maxlength: [59, "filename cannot exceed 50 characters"],
+    },
+    content: {
+      type: String,
+      required: [true, "post must contain some content"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Post", PostSchema);
