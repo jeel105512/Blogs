8210ac7a32b5e52629206067676890b1516c465c
jeel105512 <jeelhpatel10@gmail.com>
2024-03-14 19:38:54 -0400

    Like model added

---
+++ models/Like.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const LikeSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    postId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    numberOfLikes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Like", LikeSchema);
