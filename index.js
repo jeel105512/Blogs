ad90975f21f3ca61db276f0df7b70966e0f57b41
jeel105512 <jeelhpatel10@gmail.com>
2024-03-23 09:50:38 -0400

    new starter

--- index.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-
-const port = process.env.PORT || 5200;
-const server = app.listen(port, () => {
-    console.log(`API listening on port http://localhost:${port}`);
-});
-
-export default server;
